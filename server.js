583d5c494b1848d80aad2ebd40044d87d0320dcc
Alex-866 <alex.chan9923@gmail.com>
2021-04-12 08:06:52 +0000

    dd

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const express = require('express');
 const app = express();
 
 app.listen(8080, () => {
- console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
 
 app.get("/", (req, res) => {
- res.send(["Hello! This is version 0.6"]);
+    res.send(["Hello! This is version 0.6"]);
 });
 
 app.get('/alluser', (req, res) => {
-    db.userone.find().pretty() 
-        res.send (result);
+    userone.find()
+        res.json (result);
     });
 
 app.get('*',function (req, res) {
