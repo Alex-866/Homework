5a79d162c06ac1b6dc1d33d8d298a6fa5274f44e
Alex-866 <alex.chan9923@gmail.com>
2021-04-12 09:08:47 +0000

    ff

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 
 const express = require('express');
 const app = express();
+const mongoose = require ('mongoose');
+const connectionString = 'mongodb88://mongo:27017/userone';
+
+mongoose
+    .connect(connectionString, {useNewUrlParser: true })
+    .catch (e => {
+        console.error('Connection error', e.message)
+    });
 
 app.listen(8080, () => {
     console.log("Server running on port 3000");
@@ -10,6 +18,7 @@ app.get("/", (req, res) => {
     res.send(["Hello! This is version 0.6"]);
 });
 
+const db = mongoose.connection
 app.get('/alluser', (req, res) => {
     userone.find()
         res.json (result);
@@ -18,3 +27,5 @@ app.get('/alluser', (req, res) => {
 app.get('*',function (req, res) {
         res.redirect('/');
     });
+
+    module.exports = db
