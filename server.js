14c0ced187be3cd914a44d436c512e809588d16a
Alex-866 <76771646+Alex-866@users.noreply.github.com>
2021-04-12 15:06:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const app = express();
 
-app.listen(27017, () => {
-    console.log('Server Started');
+app.listen(8080, () => {
+ console.log("Server running on port 3000");
+});
+
+app.get("/", (req, res) => {
+ res.send(["Hello! This is version 0.6"]);
 });
 
 app.get('/alluser', (req, res) => {
